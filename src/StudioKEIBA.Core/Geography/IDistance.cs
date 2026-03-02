1731a7499f7bd0651fee0a792e995ae6b106c639
nkawarai <studio105.xxx@gmail.com>
2026-03-02 15:28:39 +0900

    フォルダ構成の追加。

---
+++ src/StudioKEIBA.Core/Geography/IDistance.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudioKEIBA
+{
+    /// <summary>
+    /// 距離
+    /// </summary>
+    public interface IDistance
+    {
+
+    }
+}
