c9e7b4a6f00bfcdb422a82451cde674ecaf5252b
nkawarai <studio105.xxx@gmail.com>
2026-03-17 16:14:26 +0900

    [refs #7]成績表示用のユーザーコントロールの追加。

---
+++ src/StudioKEIBA.HorseProfilerApp/Views/UCRaceResultStats.Designer.cs
@@ -0,0 +1,60 @@
+﻿namespace StudioKEIBA.HorseProfilerApp.Views
+{
+    partial class UCRaceResultStats
+    {
+        /// <summary> 
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region コンポーネント デザイナーで生成されたコード
+
+        /// <summary> 
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を 
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            _dataGridViewRaceStats = new DataGridView();
+            ((System.ComponentModel.ISupportInitialize)_dataGridViewRaceStats).BeginInit();
+            SuspendLayout();
+            // 
+            // _dataGridViewRaceStats
+            // 
+            _dataGridViewRaceStats.BackgroundColor = SystemColors.Control;
+            _dataGridViewRaceStats.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            _dataGridViewRaceStats.Dock = DockStyle.Fill;
+            _dataGridViewRaceStats.Location = new Point(0, 0);
+            _dataGridViewRaceStats.Name = "_dataGridViewRaceStats";
+            _dataGridViewRaceStats.Size = new Size(349, 75);
+            _dataGridViewRaceStats.TabIndex = 0;
+            // 
+            // UCRaceResultStats
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(_dataGridViewRaceStats);
+            Name = "UCRaceResultStats";
+            Size = new Size(349, 75);
+            ((System.ComponentModel.ISupportInitialize)_dataGridViewRaceStats).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private DataGridView _dataGridViewRaceStats;
+    }
+}
---
+++ src/StudioKEIBA.HorseProfilerApp/Views/UCRaceResultStats.cs
@@ -0,0 +1,73 @@
+﻿using StudioKEIBA.Racing;
+
+namespace StudioKEIBA.HorseProfilerApp.Views
+{
+    public partial class UCRaceResultStats : UserControl
+    {
+        public UCRaceResultStats(IRaceStats stats)
+        {
+            InitializeComponent();
+            if (stats == null) return;
+            InitializeDataGridView(stats);
+        }
+
+        private void InitializeDataGridView(IRaceStats stats)
+        {
+            _dataGridViewRaceStats.AutoGenerateColumns = false;
+            _dataGridViewRaceStats.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            _dataGridViewRaceStats.ReadOnly = true;
+            _dataGridViewRaceStats.RowHeadersVisible = false;
+            _dataGridViewRaceStats.AllowUserToResizeColumns = false;
+            _dataGridViewRaceStats.AllowUserToResizeRows = false;
+            _dataGridViewRaceStats.ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+            foreach (DataGridViewColumn column in _dataGridViewRaceStats.Columns)
+            {
+                column.SortMode = DataGridViewColumnSortMode.NotSortable;
+            }
+
+            _dataGridViewRaceStats.Columns.AddRange(new DataGridViewColumn[]
+            {
+                new DataGridViewTextBoxColumn
+                {
+                    HeaderText = "着別度数",
+                    Name = nameof(IRaceStats.DisplayString),
+                    DefaultCellStyle = new DataGridViewCellStyle
+                    {
+                        Alignment = DataGridViewContentAlignment.MiddleCenter
+                    }
+                },
+                new DataGridViewTextBoxColumn
+                {
+                    HeaderText = "勝率",
+                    Name = nameof(IRaceStats.WinRate),
+                    DefaultCellStyle = new DataGridViewCellStyle
+                    {
+                        Alignment = DataGridViewContentAlignment.MiddleCenter,
+                        Format = "P1"
+                    }
+                },
+                new DataGridViewTextBoxColumn
+                {
+                    HeaderText = "複勝率",
+                    Name = nameof(IRaceStats.Top3Rate),
+                    DefaultCellStyle = new DataGridViewCellStyle
+                    {
+                        Alignment = DataGridViewContentAlignment.MiddleCenter,
+                        Format = "P1"
+                    }
+                },
+            });
+
+            var row = new
+            {
+                DisplayString = stats.DisplayString(),
+                WinRate = stats.WinRate,
+                Top3Rate = stats.Top3Rate,
+            };
+
+            _dataGridViewRaceStats.DataSource = new[] { row }.ToList();
+            _dataGridViewRaceStats.AutoResizeColumns(DataGridViewAutoSizeColumnsMode.AllCells);
+        }
+    }
+}
